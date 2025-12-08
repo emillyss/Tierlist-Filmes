89735c2c47dd0de0300de0056c2173699d356971
Vini <42623471+vini-001@users.noreply.github.com>
2025-12-07 23:43:58 -0300

    cores, footer

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main style={{ maxWidth: 'auto', margin: '40px auto', textAlign: 'center' }}>
+    <main style={{ maxWidth: 'auto', margin: '40px auto', textAlign: 'center', color:'white' }}>
       <h1>Bem-vindo(a) à sua Tierlist de Filmes</h1>
       {children}
     </main>
